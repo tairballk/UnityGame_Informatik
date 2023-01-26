e54f80f64340050bd2a015cfc1678ac8d027d98d
Jan K <123118254+tairballk@users.noreply.github.com>
2023-01-26 12:47:30 +0100

    Import Assets
    + Tutorial aus discord von Jan angefangen
    + test gameSzene erstellt mit Plattform und Figur und erstem PlayerMovement Script
    - Charakter läuft noch nicht

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Joystick MovingJoystick;
+
+    float moveSpeed = 4.5f;
+    public Transform MovingArrow;
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); //using this later to change the default animations
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovingArrow.rotation = MovingArrow.rotation = Quaternion.Euler(0f, Mathf.Atan2(MovingJoystick.Horizontal, MovingJoystick.Vertical) * Mathf.Rad2Deg, 0f);
+        if (Mathf.Abs(MovingJoystick.Horizontal) > .1f || Mathf.Abs(MovingJoystick.Vertical) > .1f) {
+
+            if(!MovingArrow.gameObject.activeSelf)
+                MovingArrow.gameObject.SetActive(true);
+
+            transform.LookAt(new Vector3(MovingJoystick.Horizontal + transform.position.x, 0, MovingJoystick.Vertical + transform.position.z));
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            anim.SetBool("isRunning", true);
+
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+            if (MovingArrow.gameObject.activeSelf)
+                MovingArrow.gameObject.SetActive(false);
+        }
+
+
+    }
+}
